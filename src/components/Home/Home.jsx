14d26dea7e26bd2c8571f07030ecd00a6410a751
Tetsu <119629763+j-Hop@users.noreply.github.com>
2024-04-10 13:19:12 +0300

    u

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,60 +1,56 @@
 import SpriteIcon from '../../images/sprite.svg';
 import HeroImg from '../../images/Rectangle 1.png';
-import { Header } from '../Header/Header';
 import { useState } from 'react';
-import { 
-    BtnStart,
-    BoxCheck,
-    HomeFlexBox,
-    HomeImg,
-    HomeInfo,
-    Section,
-    IconCheck,
-    InfoHomeBox
+import {
+  BtnStart,
+  BoxCheck,
+  HomeFlexBox,
+  HomeImg,
+  HomeInfo,
+  Section,
+  IconCheck,
+  InfoHomeBox,
 } from './Home.styled';
 
-
 export const Home = () => {
-    const [changeHover, setChangeHover] = useState(false);
-    
-    const handleToggleArrow = () =>{
-        setChangeHover(prevState => !prevState);
-     };
+  const [changeHover, setChangeHover] = useState(false);
 
-    return(
-        <Section>
-            <Header/>
-            <HomeFlexBox>
-            <HomeInfo>
-                <h2>Make Life Easier for the Family:</h2>
-                <p>Find Babysitters Online for All Occasions</p>
-                <BtnStart to='/nannies' onChange={handleToggleArrow}>
-                 <span>Get started</span>
-                 <svg width='5' height='22'>
-                    <use
-                    xlinkHref={
-                    changeHover?
-                    `${SpriteIcon}#icon-Arrow-up` : `${SpriteIcon}#icon-Arrow-inline`
-                    }
-                    />
-                 </svg>
-                </BtnStart>
-            </HomeInfo>
-            <HomeImg src={HeroImg} alt='home'/>
-            <InfoHomeBox>
-                <BoxCheck>
-                    <IconCheck>
-                        <svg width='20' height='15'>
-                            <use
-                            xlinkHref={`${SpriteIcon}#icon-check`}
-                            />
-                        </svg>
-                    </IconCheck>
-                </BoxCheck>
-                <p>Experienced nannies</p>
-                <h2>15,000</h2>
-            </InfoHomeBox>
-            </HomeFlexBox>
-        </Section>
-    );
+  const handleToggleArrow = () => {
+    setChangeHover((prevState) => !prevState);
+  };
+
+  return (
+    <Section>
+      <HomeFlexBox>
+        <HomeInfo>
+          <h2>Make Life Easier for the Family:</h2>
+          <p>Find Babysitters Online for All Occasions</p>
+          <BtnStart to="/nannies" onChange={handleToggleArrow}>
+            <span>Get started</span>
+            <svg width="5" height="22">
+              <use
+                xlinkHref={
+                  changeHover
+                    ? `${SpriteIcon}#icon-Arrow-up`
+                    : `${SpriteIcon}#icon-Arrow-inline`
+                }
+              />
+            </svg>
+          </BtnStart>
+        </HomeInfo>
+        <HomeImg src={HeroImg} alt="home" />
+        <InfoHomeBox>
+          <BoxCheck>
+            <IconCheck>
+              <svg width="20" height="15">
+                <use xlinkHref={`${SpriteIcon}#icon-check`} />
+              </svg>
+            </IconCheck>
+          </BoxCheck>
+          <p>Experienced nannies</p>
+          <h2>15,000</h2>
+        </InfoHomeBox>
+      </HomeFlexBox>
+    </Section>
+  );
 };
