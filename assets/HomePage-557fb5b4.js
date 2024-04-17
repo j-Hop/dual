cd2ee1779957fc88e1296aceab0ab48f54e9852b
j-Hop <119629763+j-Hop@users.noreply.github.com>
2024-04-17 08:13:43 +0000

    Deploying to gh-pages from @ j-Hop/dual@782b4924b0bdb8f7202248fd3166ff6357d112ad 🚀

---
+++ assets/HomePage-557fb5b4.js
@@ -0,0 +1,91 @@
+import{s as e,t as i,N as s,j as n,S as o}from"./index-e6b899b8.js";const r="/dual/assets/Rectangle 1-2557fb2d.png",t=e.div`
+  padding: ${i.spacing(32)} ${i.spacing(48)};
+  inline-size: 1380px;
+  background-color: ${i.colors.green};
+  block-size: 736px;
+  border-radius: ${i.radius.xl};
+`,l=e.div`
+  position: absolute;
+  max-inline-size: 530px;
+  top: 251px;
+
+  h2 {
+    font-size: 70px;
+    font-weight: 500;
+    line-height: 1;
+    color: ${i.colors.light};
+    margin-block-end: ${i.spacing(14)};
+  }
+
+  p {
+    font-size: 28px;
+    font-weight: 400;
+    line-height: 1.07;
+    color: ${i.colors.light};
+    margin-block-end: ${i.spacing(32)};
+  }
+`,a=e.img`
+  height: 736px;
+  max-width: 699px;
+  transform: translate(585px, -64px);
+`,c=e(s)`
+  border: 1px solid rgba(251, 251, 251, 0.4);
+  border-radius: ${i.radius.xl};
+  background-color: transparent;
+  padding: ${i.spacing(9)} ${i.spacing(25)};
+  display: inline-block;
+  transition: border ${i.transition};
+  display: flex;
+  align-items: center;
+  max-inline-size: 235px;
+
+  &:hover {
+    border: ${i.colors.light};
+  }
+
+  span {
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 1.2;
+    color: ${i.colors.light};
+    margin-inline-end: ${i.spacing(9)};
+  }
+`,d=e.div`
+  padding: ${i.spacing(16)};
+  border: none;
+  border-radius: 20px;
+  background-color: ${i.colors.light};
+  position: relative;
+  left: 937px;
+  bottom: 230px;
+  inline-size: 290px;
+  display: flex;
+
+  h2 {
+    color: ${i.colors.black};
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 1.2;
+  }
+
+  p {
+    color: rgba(17, 16, 28, 0.5);
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.2;
+    margin: 0;
+  }
+`,p=e.div`
+  border: none;
+  background-color: ${i.colors.green};
+  border-radius: 13px;
+  margin-inline-end: ${i.spacing(8)};
+  width: 54px;
+  height: 54px;
+  padding: ${i.spacing(9.75)} ${i.spacing(8.5)};
+`,g=e.div`
+display: flex;
+flex-direction: column;
+gap: ${i.spacing(3)};
+}
+`,x=()=>n.jsxs(t,{children:[n.jsxs(l,{children:[n.jsx("h2",{children:"Make Life Easier for the Family:"}),n.jsx("p",{children:"Find Babysitters Online for All Occasions"}),n.jsxs(c,{to:"/nannies",children:[n.jsx("span",{children:"Get started"}),n.jsx("svg",{width:"15",height:"22",children:n.jsx("use",{xlinkHref:`${o}#icon-Arrow-inline`})})]})]}),n.jsx(a,{src:r,alt:"home"}),n.jsxs(d,{children:[n.jsx(p,{children:n.jsx("svg",{width:"20",height:"15",children:n.jsx("use",{xlinkHref:`${o}#icon-check`})})}),n.jsxs(g,{children:[n.jsx("p",{children:"Experienced nannies"}),n.jsx("h2",{children:"15,000"})]})]})]});function f(){return n.jsx(x,{})}export{f as default};
